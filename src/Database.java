454c6299d576c94a60f21147cf4190a3cd476c29
agebers <178644290+agebers@users.noreply.github.com>
2024-08-17 23:33:56 +0200

    prepared statements added

---
+++ src/Database.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+
+public class Database {
+    private String dbUrl;
+    private String dbUsername;
+    private String dbPassword;
+    private Connection dbCon;
+    ResultSet resultSet;
+    private PreparedStatement preparedStatement;
+
+    public Database(String databaseName, String username, String password, String databasePort) {
+        dbUrl = "jdbc:mariadb://localhost:" + databasePort + "/" + databaseName;
+        dbUsername = username;
+        dbPassword = password;
+    }
+
+    public String getMovieTitle(int id) throws SQLException {
+        open();
+        String sqlQuery = "SELECT title FROM movie WHERE id = ?";
+        preparedStatement = dbCon.prepareStatement(sqlQuery);
+        preparedStatement.setInt(1, id);
+        resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        close();
+        return resultSet.getString("title");
+    }
+
+    public void open() throws SQLException {
+        try {
+            dbCon = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() throws SQLException {
+        dbCon.close();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,13 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 public class Main {
-    private static final String dbUrl = "jdbc:mariadb://localhost:3306/javademo";
+    private static final String databaseName = "javademo";
     private static final String dbUsername = "myuser";
     private static final String dbPassword = "secret";
+    private static final String dbPort = "3306";
+    private static Database database;
 
     public static void main(String[] args) throws Exception {
-        String sqlQuery = "select title from movie where id = 1";
-
-        Connection dbCon = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-        Statement stmt = dbCon.createStatement();
-        ResultSet result = stmt.executeQuery(sqlQuery);
-        result.next();
-        String title = result.getString(1);
+        database = new Database(databaseName, dbUsername, dbPassword, dbPort);
+        String title = database.getMovieTitle(1);
         System.out.println(title);
-        dbCon.close();
     }
 }
